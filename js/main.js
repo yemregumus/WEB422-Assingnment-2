371ff58ee721c172ef47dcc1e35d0b0e081e7eb9
yemregumus <ygumus@myseneca.ca>
2023-09-29 19:47:39 -0400

    last check

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Yunus Gumus Student ID: 150331197 Date:
+*  Name: Yunus Gumus Student ID: 150331197 Date: 2023/09/29
 *
 ********************************************************************************/ 
 
